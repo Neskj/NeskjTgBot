04c1b191ff3e668eb1ef6beb0ccc879b60d18c70
Neskj <pronin.qq@gmail.com>
2024-05-12 00:29:04 +0300

    create directory

---
+++ NeskjTgBot/src/main/java/neskj/NeskjTgBot/NeskjTgBotApplication.java
@@ -0,0 +1,13 @@
+package neskj.NeskjTgBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeskjTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeskjTgBotApplication.class, args);
+	}
+
+}
---
+++ NeskjTgBot/src/test/java/neskj/NeskjTgBot/NeskjTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package neskj.NeskjTgBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeskjTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
