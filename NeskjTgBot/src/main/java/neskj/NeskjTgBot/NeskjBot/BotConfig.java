2e984ab84eb98d610915fd496d07bc55951cfdb3
Neskj <pronin.qq@gmail.com>
2024-05-13 06:56:57 +0300

    Bot is work now

--- NeskjTgBot/src/main/java/neskj/NeskjTgBot/NeskjBot/BotConfig.java
+++ NeskjTgBot/src/main/java/neskj/NeskjTgBot/NeskjBot/BotConfig.java
@@ -1,4 +1,33 @@
 package neskj.NeskjTgBot.NeskjBot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Component
 public class BotConfig {
+
+    private final NeskjBot neskjBot;
+
+    @Autowired
+    public BotConfig(NeskjBot neskjBot){
+        this.neskjBot=neskjBot;
+    }
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void initBot(){
+
+        try {
+            TelegramBotsApi telegramBotsApi=new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(neskjBot);
+        } catch (TelegramApiException e){
+            System.out.println("-->> Exception while try to init bot <<--");
+        }
+
+
+    }
 }
--- NeskjTgBot/src/main/java/neskj/NeskjTgBot/NeskjBot/NeskjBot.java
+++ NeskjTgBot/src/main/java/neskj/NeskjTgBot/NeskjBot/NeskjBot.java
@@ -1,9 +1,13 @@
 package neskj.NeskjTgBot.NeskjBot;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Component
 public class NeskjBot extends TelegramLongPollingBot {
 
     @Value("${bot.name}")
@@ -23,6 +27,32 @@ public class NeskjBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+
+        if(update.hasMessage()&&update.getMessage().hasText()) {
+            String incomingMessage=update.getMessage().getText();
+            long chatID=update.getMessage().getChatId();
+
+            if(incomingMessage.equals("/hello")){
+                sendMessage(chatID,"Hello world im a new Neskj's bot");
+            }else{
+                sendMessage(chatID,"Not understand you");
+            }
+        }
+    }
+
+    private void sendMessage(long chatId,String text){
+
+        SendMessage responseMessage=new SendMessage();
+        responseMessage.setChatId(chatId);
+        responseMessage.setText(text);
+
+        try{
+            execute(responseMessage);
+        }catch (TelegramApiException e){
+            System.out.println("-->> Exception while try to send response to user message <<--");
+        }finally {
+            System.out.println("-->> I send a new message: "+responseMessage+"<<--");
+        }
 
     }
 }
