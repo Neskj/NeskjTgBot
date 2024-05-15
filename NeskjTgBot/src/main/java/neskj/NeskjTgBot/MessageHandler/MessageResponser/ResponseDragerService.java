10c434026de5c61a7c8f1bab48fda3c418c53a2d
Neskj <pronin.qq@gmail.com>
2024-05-15 12:52:35 +0300

    update answer for drager

--- NeskjTgBot/src/main/java/neskj/NeskjTgBot/MessageHandler/MessageResponser/ResponseDragerService.java
+++ NeskjTgBot/src/main/java/neskj/NeskjTgBot/MessageHandler/MessageResponser/ResponseDragerService.java
@@ -4,6 +4,8 @@ public class ResponseDragerService implements MessageResponser{
 
     @Override
     public String responseTheMessage() {
-        return "вы выбрали дрегер сервис";
+        return "Сервис метрологических данных газоанализаторов.\n\nАдрес: http://185.198.152.105:9096/drager \n\nгостевой аккаунт: \nлогин: ctd\nпароль: 12345 \n\nГитХаб: https://github.com/Neskj/DragerService \n\n"+
+                "Проект позволяет обеспечить метрологический контроль за используемыми газоанализаторами, их своевременную отправку на поверку/калибровку.\nСостояния : \n* окрашен красным - прибор просрочен\n* окрашен желтым - сдать в течение 5 дней \n\n"+
+                "Использованы технологии: \nJDBC, Postgresql, фронтэнд: HTML+CSS+Thymeleaf ";
     }
 }
