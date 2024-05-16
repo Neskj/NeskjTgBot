7273159c03a3e6cb9a9a655206553a5dc3d60933
Neskj <pronin.qq@gmail.com>
2024-05-16 06:56:34 +0300

    update message for generator

--- NeskjTgBot/src/main/java/neskj/NeskjTgBot/MessageHandler/MessageResponser/ResponseWeatherApp.java
+++ NeskjTgBot/src/main/java/neskj/NeskjTgBot/MessageHandler/MessageResponser/ResponseWeatherApp.java
@@ -5,6 +5,7 @@ public class ResponseWeatherApp implements MessageResponser{
     @Override
     public String responseTheMessage() {
         return "Погодный сервис.\n\nАдрес: http://185.198.152.105:8086/weather \n\nГитХаб: https://github.com/Neskj/ProjectWeatherApp \n\nCоздан на основе открытого API openweathermap." +
-                " В нем использована технология Open Feign из состава Spring Cloud. Добавлена REST точка для возможности получать данные от сервиса из сторонних приложений.\n";
+                " В нем использована технология Open Feign из состава Spring Cloud. Добавлена REST точка для возможности получать данные от сервиса из сторонних приложений.\n\n"+
+                "Использованы технологии: \nJava 21, Spring Boot/Cloud, REST Api , фронтэнд: HTML+CSS+Thymeleaf ";
     }
 }
