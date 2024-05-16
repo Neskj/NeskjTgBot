e329bb23ec9ba47f79108840e72ba5259bb2c08d
Neskj <pronin.qq@gmail.com>
2024-05-16 12:42:21 +0300

    update info for start

--- NeskjTgBot/src/main/java/neskj/NeskjTgBot/MessageHandler/MessageResponser/ResponseStart.java
+++ NeskjTgBot/src/main/java/neskj/NeskjTgBot/MessageHandler/MessageResponser/ResponseStart.java
@@ -4,6 +4,8 @@ public class ResponseStart implements MessageResponser{
 
     @Override
     public String responseTheMessage(){
-        return "Привет! вы выбрали /start";
+        return "Реализованные проекты:\n\n*  Погодный сервис /weather" +
+                "\n\n*  Метрологический info-сервис по газоанализаторам /drager" +
+                "\n\n*  Веб генератор QR пропуска на предприятие /generator\n\n*  Информационный бот /bot";
     }
 }
